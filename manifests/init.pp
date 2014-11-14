5e53cdc9003e90696ec3af28e06b611063a18b9c
lawrencetaylor <lozengivity@gmail.com>
2014-11-14 13:27:03 +0000

    Disable starting of site

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,15 +10,4 @@ class nodesample() {
 		command => "/usr/local/node/node-default/bin/npm install --prefix /home/vagrant/SampleSite express",
 		timeout => 0
 	}
-
-	exec {"Install forever":
-		command => "/usr/bin/sudo /usr/local/node/node-default/bin/npm install forever -g",
-		timeout => 0
-	}
-	
-	exec {"Start site":
-		command => "/usr/local/node/node-default/bin/forever start /home/vagrant/SampleSite/index.js",
-		timeout => 0,
-		require => [ Exec["Download sample"], Exec["Install express"], Exec["Install forever"] ]
-	}
 }
